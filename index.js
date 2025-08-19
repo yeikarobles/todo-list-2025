a0a00a41496c331ccbf04f828762c8973e25250b
yeikarobles <yeika.robles.v@gmail.com>
2025-08-18 22:54:39 -0400

    organized code structure

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 //variables del html
+let addButton;
+let dataInput;
+let cancelButton;
 
-//funciones
+document.addEventListener("DOMContentLoaded", () => {
+  addButton = document.getElementById("addTask");
+  dataInput = document.getElementById("newTask");
+  cancelButton = document.getElementById("");
+
+  dataInput.hidden = true;
+  addButton.addEventListener("click", onAddButtonClick);
+});
+
+//funciones::
+
+//add task
+//cambia la propiedad hidden de la tabla new task
+function onAddButtonClick() {
+  dataInput.hidden = false;
+}
+
+function onCancelButtonClick() {
+  dataInput.hidden = true;
+}
 
 //create
+
 //read
+
 //update
+
 //delete
