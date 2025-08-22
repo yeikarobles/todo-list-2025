8d2b29d8469ab35f459d26b8b63cbef0b670d699
yeikarobles <yeika.robles.v@gmail.com>
2025-08-22 17:44:42 -0400

    testing json local storage

--- index.js
+++ index.js
@@ -3,31 +3,64 @@ let addButton;
 let dataInput;
 let cancelButton;
 
+let todos = [];
+
 document.addEventListener("DOMContentLoaded", () => {
   addButton = document.getElementById("addTask");
   dataInput = document.getElementById("newTask");
-  cancelButton = document.getElementById("");
+  cancelButton = document.getElementById("cancelButton");
 
   dataInput.hidden = true;
   addButton.addEventListener("click", onAddButtonClick);
+  cancelButton.addEventListener("click", onCancelButtonClick);
+
+  var saved = GetFromLocal("item");
+
+  if (!saved) {
+    const jsonTodos = JSON.stringify(todos);
+    SaveOnLocal("item", jsonTodos);
+  } else {
+    todos = JSON.parse(saved);
+    console.log(todos);
+  }
 });
 
-//funciones::
-
-//add task
 //cambia la propiedad hidden de la tabla new task
 function onAddButtonClick() {
   dataInput.hidden = false;
 }
-
 function onCancelButtonClick() {
   dataInput.hidden = true;
 }
+//add
+function onSaveButtonClick() {}
 
-//create
+const task = {
+  id: "",
+  name: "",
+  priority: "",
+  status: "",
+  dueDate: "",
+  daysLeft: "",
+  category: "",
+  notes: "",
+};
 
-//read
+function SaveOnLocal(name, value) {
+  localStorage.setItem(name, value);
+}
 
-//update
+function GetFromLocal(name) {
+  var stg = localStorage.getItem(name);
+  if (stg) return stg;
+  return undefined;
+}
 
-//delete
+// asignarle id a cada input que hara el usuario para asignar los datos
+// save button = agregar elemento al array
+
+// crear un array donde se almacene la informacion que ingresa el usuario
+
+// tener un json que almacene la informacion de la lista del html
+
+// que por cada elemento de la lista sea creado una nueva row en la table
